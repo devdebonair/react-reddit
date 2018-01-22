db31aeb5c72f7f50508e752054c86a35ae11d7b8
Vincent Moore <devdebonair@gmail.com>
2018-01-22 00:51:30 -0500

    added hot reloading to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const port = process.env.PORT || 3000;
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: ['react-hot-loader/patch', './src/index.js'],
 	output: {
-		filename: 'bundle.[hash].js'
+		filename: 'bundle.[hash].js',
+		publicPath: "/"
 	},
 	devtool: "inline-source-map",
 	module: {
@@ -38,12 +39,16 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: 'public/index.html',
 			favicon: 'public/favicon.ico'
-		})
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
 	],
 	devServer: {
 		host: 'localhost',
 		port: port,
 		historyApiFallback: true,
-		open: true
+		open: true,
+		hot: true
 	}
 };
