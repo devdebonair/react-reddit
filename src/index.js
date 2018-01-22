869c40899c3a2f1c532a6066d01e232d4cc26c4b
Vincent Moore <devdebonair@gmail.com>
2018-01-22 01:04:17 -0500

    added hot reloading to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { AppContainer } from "react-hot-loader"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 
-render(
-    <App />,
-    document.getElementById('root')
-);
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById('root')
+    )
+}
+
+render(App)
+
+// Webpack Hot Module Replacement API
+if (module.hot) module.hot.accept('./components/App', () => render(App))
